337e0524f13f58ae65b57f01044be135f2b4e2d3
vcrolack <victorolack@gmail.com>
2022-03-16 22:25:50 -0300

    ui: formulario de login agregado con sus validaciones

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-login',
@@ -7,9 +8,43 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  loginForm: FormGroup;
+
+  mensajesErrores = {
+    email: [
+      {type: "required", message: "El email es obligatorio."},
+      {type: "pattern", message: "El email ingresado no es válido."}
+    ],
+    contrasena: [
+      {type: "required", message: "La contraseña es obligatoria."},
+      {type: "maxlength", message: "La contraseña debe de tener 30 caracteres máximo"},
+      {type: "minlength", message: "La contraseña debe de tener 5 caracteres mínimo"}
+    ]
+  }
+
+  constructor(private formBuilder: FormBuilder) {
+    this.loginForm = this.formBuilder.group({
+
+      email: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.pattern("[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,3}$"),
+      ])
+      ),
+      contrasena: new FormControl("", Validators.compose([
+        Validators.required,
+        Validators.minLength(5),
+        Validators.maxLength(30),
+      ])
+      )
+    })
+
+  }
 
   ngOnInit(): void {
+  }
+
+  loginUser(data: any) {
+    console.log('sesion iniciada')
   }
 
 }
